6a9fffd8358d9f54fa60f4254f4cd15a3f8929bf
dadanabdilah <abdilahdadan5@gmailcom>
2023-02-08 19:27:50 +0700

    add dashboard info

--- app/Views/dashboard/index.php
+++ app/Views/dashboard/index.php
@@ -23,7 +23,51 @@
 
         <?php if (session('message') !== null) : ?>
             <div class="alert alert-success" role="alert"><?= session('message') ?></div>
-        <?php endif ?>    
+        <?php endif ?>
+        
+        <div class="row">
+          <div class="col-lg-3 col-6">
+            <!-- small box -->
+            <div class="small-box bg-info">
+              <div class="inner">
+                <h3><?= $produk ?></h3>
+
+                <p>Total Data Produk</p>
+              </div>
+              <div class="icon">
+                <i class="ion ion-bag"></i>
+              </div>
+            </div>
+          </div>
+          <!-- ./col -->
+          <div class="col-lg-3 col-6">
+            <!-- small box -->
+            <div class="small-box bg-success">
+              <div class="inner">
+                <h3><?= $resi ?></h3>
+
+                <p>Total Data Resi</p>
+              </div>
+              <div class="icon">
+                <i class="ion ion-stats-bars"></i>
+              </div>
+            </div>
+          </div>
+          <!-- ./col -->
+          <div class="col-lg-3 col-6">
+            <!-- small box -->
+            <div class="small-box bg-warning">
+              <div class="inner">
+                <h3><?= $admin ?></h3>
+
+                <p>Total Data Admin</p>
+              </div>
+              <div class="icon">
+                <i class="ion ion-person-add"></i>
+              </div>
+            </div>
+          </div>
+        </div>
     </div><!-- /.container-fluid -->
 </section>
 <!-- /.content -->
