7bb472408b29d9a035aa7620f6c38f7d06fc2b99
Mohammad Irwansyah Somantri <mirwansyahs@MacBook-Pro-mohammad.local>
2023-02-15 13:25:17 +0700

    Fixing

--- app/Controllers/Sistem.php
+++ app/Controllers/Sistem.php
@@ -87,7 +87,7 @@ class Sistem extends BaseController
                 }
             } else if(@$result->status->code == 400){
                 $deskripsi = $result->status->description;
-
+                var_dump($result);
                 $data = [
                     'resi_id' => $values->resi_id,
                     'deskripsi' => $deskripsi,
@@ -99,7 +99,7 @@ class Sistem extends BaseController
                 $message .= "\r\n*Pembelian : " . $this->Produk->where('kode_barang', $values->kode_barang)->first()->nama_barang . "*";
                 $message .= "\r\n*No resi : " . $values->no_resi . "*";
                 $message .= "\r\n*Keterangan : " . $deskripsi . "*";
-                $message .= "\r\n*Update Resi: Kurir telah pick up paket*";
+                // $message .= "\r\n*Update Resi: ".  ."*";
                 $message .= "\r\n_Ini adalah pesan otomatis, tolong jangan balas pesan ini, jika ada pertanyaan langsung tanyakan ke admin yaa :))_";
 
                 // $message = "Halo kak, berikut informasi dari resi kaka :\r\nKeterangan : " . $deskripsi;
